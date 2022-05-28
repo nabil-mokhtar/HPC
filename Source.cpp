51dd48a60ae0b762fcb70d5c485f30d24f8b9a66
Nabil Mokhtar <nabilmokhtar15@gmail.com>
2022-05-29 01:21:37 +0200

    mpi

--- Source.cpp
+++ Source.cpp
@@ -5,6 +5,7 @@
 //#include<bits/stdc++.h>
 #include<msclr\marshal_cppstd.h>
 #include <ctime>// include this header 
+#include<mpi.h>
 #pragma once
 
 #using <mscorlib.dll>
@@ -30,10 +31,10 @@ int* inputImage(int* w, int* h, System::String^ imagePath) //put the size of ima
 	OriginalImageHeight = BM.Height;
 	*w = BM.Width;
 	*h = BM.Height;
-	int* Red = new int[BM.Height * BM.Width];
-	int* Green = new int[BM.Height * BM.Width];
-	int* Blue = new int[BM.Height * BM.Width];
-	input = new int[BM.Height * BM.Width];
+	int *Red = new int[BM.Height * BM.Width];
+	int *Green = new int[BM.Height * BM.Width];
+	int *Blue = new int[BM.Height * BM.Width];
+	input = new int[BM.Height*BM.Width];
 	for (int i = 0; i < BM.Height; i++)
 	{
 		for (int j = 0; j < BM.Width; j++)
@@ -44,7 +45,7 @@ int* inputImage(int* w, int* h, System::String^ imagePath) //put the size of ima
 			Blue[i * BM.Width + j] = c.B;
 			Green[i * BM.Width + j] = c.G;
 
-			input[i * BM.Width + j] = ((c.R + c.B + c.G) / 3); //gray scale value equals the average of RGB values
+			input[i*BM.Width + j] = ((c.R + c.B + c.G) / 3); //gray scale value equals the average of RGB values
 
 		}
 
@@ -63,15 +64,15 @@ void createImage(int* image, int width, int height, int index)
 		for (int j = 0; j < MyNewImage.Width; j++)
 		{
 			//i * OriginalImageWidth + j
-			if (image[i * width + j] < 0)
+			if (image[i*width + j] < 0)
 			{
-				image[i * width + j] = 0;
+				image[i*width + j] = 0;
 			}
-			if (image[i * width + j] > 255)
+			if (image[i*width + j] > 255)
 			{
-				image[i * width + j] = 255;
+				image[i*width + j] = 255;
 			}
-			System::Drawing::Color c = System::Drawing::Color::FromArgb(image[i * MyNewImage.Width + j], image[i * MyNewImage.Width + j], image[i * MyNewImage.Width + j]);
+			System::Drawing::Color c = System::Drawing::Color::FromArgb(image[i*MyNewImage.Width + j], image[i*MyNewImage.Width + j], image[i*MyNewImage.Width + j]);
 			MyNewImage.SetPixel(j, i, c);
 		}
 	}
@@ -84,7 +85,7 @@ int calc_l(int n) {
 }
 
 
-int find_median(int* a, int size) {
+int find_median(int* a ,int size) {
 	//sort(arr, arr + size);
 	int i, j, temp;
 
@@ -97,14 +98,17 @@ int find_median(int* a, int size) {
 				a[j] = temp;
 			}
 		}
-
+		
 	}
 
-	//cout << arr[size / 2]<<endl;
+	
 
 	return a[size / 2];
 
 }
+
+
+
 
 
 int main()
@@ -122,67 +126,96 @@ int main()
 
 
 	start_s = clock();
-	int filter_size = 9;
+
+
+	int filter_size =9;
 	int levels = calc_l(filter_size);
 
-	// new array size 
+	int* filter = new int[filter_size] ;
 
-	int p_hight = ImageHeight + 2 * levels;
-	int p_width = ImageWidth + 2 * levels;
-	int p_size = p_hight * p_width;
+	
+	// mpi init
+	MPI_Init(NULL, NULL);
+	int world_size;
+	MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+	int world_rank;
+	MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+	int partial_size = ImageHeight * ImageWidth / world_size;
 
-	int* pad_image = new int[p_size] {0};
+	cout << "world size : " << world_size << " partial : " << partial_size<<endl;
+	int start = partial_size * world_rank;   //foreach process
+	int end = start + partial_size - 1;      //foreach process
+	int* partial_image = new int[partial_size] ;
 
-	int* filter = new int[filter_size];
+	int row = ImageHeight / world_size;
+	int size = (row + 2 * levels) * (ImageWidth + 2 * levels);
+	int* partial_image_p = new int[size]{0};
+
+	
+
+
+	MPI_Scatter(imageData, partial_size, MPI_INT, partial_image, partial_size, MPI_INT, 0, MPI_COMM_WORLD);
 
 
 
-	for (int i = levels; i < p_hight - levels; i++)
+	// add pading to image
+	for (int i = levels; i < row + levels; i++)
 	{
-		for (int j = levels; j < p_width - levels; j++)
+		for (int j = levels; j < ImageWidth + levels; j++)
 		{
-			pad_image[p_width * i + j] = imageData[ImageWidth * (i - levels) + (j - levels)];
+			partial_image_p[ImageWidth * i + j] = partial_image[ImageWidth * (i - levels) + (j - levels)];
 		}
 	}
 
 
-
-	for (int i = levels; i < p_hight - levels; i++)
+	// filter
+	for (int i = levels; i < row+levels; i++)
 	{
 
-		for (int j = levels; j < p_width - levels; j++)
+		for (int j = levels; j < ImageWidth+levels; j++)
 		{
-
+			
 			int counter = 0;
-			for (int h = i - levels; h <= i + levels; h++)
+
+			// loop for filter 
+			for (int h = i-levels; h <= i+levels ; h++)
 			{
-				for (int w = j - levels; w <= j + levels; w++)
+				for (int w =j-levels; w <= j+levels ; w++)
 				{
-					filter[counter] = pad_image[p_width * h + w];
+					filter[counter] = partial_image_p[ImageWidth * h + w];
 					counter++;
 				}
 			}
-			imageData[ImageWidth * (i - levels) + (j - levels)] = find_median(filter, filter_size);
+			partial_image[ImageWidth * i -levels + j- levels]=find_median(filter,filter_size);
 
 		}
-
+	
 	}
 
-	createImage(imageData, ImageWidth, ImageHeight, 0);
+
+	MPI_Gather(partial_image, partial_size, MPI_INT, imageData, partial_size, MPI_INT, 0, MPI_COMM_WORLD);
+
+
+	
 	stop_s = clock();
+
+	
+	if (world_rank == 0) {
+		createImage(imageData, ImageWidth, ImageHeight, 0);
+	}
+
+	MPI_Finalize();
+	
 
 	TotalTime += (stop_s - start_s) / double(CLOCKS_PER_SEC) * 1000;
 	cout << "time: " << TotalTime << endl;
 	system("pause");
 
 	free(imageData);
-	free(pad_image);
-	free(filter);
+	delete[] filter;
 	return 0;
 
 }
-
-
 
 
 
